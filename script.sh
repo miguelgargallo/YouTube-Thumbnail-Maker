f89c23b8fc1680127b0e8d254bdfd4752e0e6c49
Miguel Gargallo <mgargallo16@gmail.com>
2022-12-27 16:36:50 +0100

    1.0.2 Improvements

--- script.sh
+++ script.sh
@@ -1,16 +1,18 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# check if required libraries are installed
-pip freeze | grep pillow > /dev/null
-if [ $? -ne 0 ]; then
-  echo "Pillow library not found. Installing..."
-  pip install pillow
-else
-  echo "Pillow library found."
-fi
+# Get the current date in the format YYYY-MM-DD
+date=$(date +"%Y-%m-%d")
 
-# create an empty image with the specified dimensions
-python -c "from PIL import Image; img = Image.new('RGB', (1280, 720), color='white'); img.save('image.jpg', 'JPEG')"
+# Generate a pastel random color
+color=$(convert -list color | sort -R | head -n 1)
 
-# draw the title on the image using the specified font and size
-python -c "from PIL import ImageFont, ImageDraw; img = Image.open('image.jpg'); draw = ImageDraw.Draw(img); font = ImageFont.truetype('arial.ttf', size=48); text = 'XL Title'; text_width, text_height = draw.textsize(text, font=font); draw.text(((1280 - text_width) / 2, (720 - text_height) / 2), text, font=font, fill=(0, 0, 0)); img.save('image.jpg', 'JPEG')"
+# Create the image with the specified dimensions and the pastel random color as the background
+convert -size 1280x720 xc:"$color" image.jpg
+
+# Add the text to the center of the image in the opposite complementary color
+convert image.jpg -gravity center -font arial -pointsize 72 -fill black -annotate +0+0 'This is a test' -channel RGBA -negate image.jpg
+
+# Save the image with the current date as the filename
+mv image.jpg "${date}.jpg"
+
+echo "Image created successfully and saved as ${date}.jpg"
