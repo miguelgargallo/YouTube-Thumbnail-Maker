e8ae706cc5132cf4b76669d55123a923ed0c7a8f
Miguel Gargallo <mgargallo16@gmail.com>
2022-12-27 17:09:06 +0100

    1.0.4 Code improvements

--- script.sh
+++ script.sh
@@ -15,9 +15,8 @@ fi
 # Draw a 1280 x 720 pixels white background, with a 16:9 aspect ratio in JPG
 python -c "from PIL import Image; Image.new('RGB', (1280, 720), (255, 255, 255)).save('image.jpg')"
 
-# Add a sentence to the image in the middle of the image (centered) by the user from a prompt
-read -p "Enter a sentence: " sentence
-python -c "from PIL import Image, ImageDraw, ImageFont; image = Image.open('image.jpg'); draw = ImageDraw.Draw(image); font = ImageFont.truetype('arial.ttf', 32); draw.text((640, 360), '$sentence', (0, 0, 0), font=font); image.save('image.jpg')"
+# Add a sentence to the image in bold, 36px in the middle of the image (centered) by the user from a prompt
+python -c "from PIL import Image, ImageDraw, ImageFont; image = Image.open('image.jpg'); draw = ImageDraw.Draw(image); font = ImageFont.truetype('arial.ttf', 36); draw.text((640, 360), input('Enter a sentence: '), (0, 0, 0), font=font, anchor='mm'); image.save('image.jpg')"
 
 # Save the image as "image.jpg" in the current directory
 python -c "import imageio; imageio.imwrite('image.jpg', imageio.imread('image.jpg'))"
