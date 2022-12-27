662a9353f1a6baf3cc913521d2608764ae6d7178
Miguel Gargallo <mgargallo16@gmail.com>
2022-12-27 16:23:07 +0100

    1.0.1 Add Pillow

--- script.sh
+++ script.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-# install required libraries
-pip install pillow
+# check if required libraries are installed
+pip freeze | grep pillow > /dev/null
+if [ $? -ne 0 ]; then
+  echo "Pillow library not found. Installing..."
+  pip install pillow
+else
+  echo "Pillow library found."
+fi
 
 # create an empty image with the specified dimensions
-from PIL import Image
-img = Image.new('RGB', (1280, 720), color='white')
+python -c "from PIL import Image; img = Image.new('RGB', (1280, 720), color='white'); img.save('image.jpg', 'JPEG')"
 
 # draw the title on the image using the specified font and size
-from PIL import ImageFont, ImageDraw
-draw = ImageDraw.Draw(img)
-font = ImageFont.truetype('arial.ttf', size=48)
-text = "XL Title"
-text_width, text_height = draw.textsize(text, font=font)
-draw.text(((1280 - text_width) / 2, (720 - text_height) / 2), text, font=font, fill=(0, 0, 0))
-
-# save the image as a JPEG file
-img.save('image.jpg', 'JPEG')
+python -c "from PIL import ImageFont, ImageDraw; img = Image.open('image.jpg'); draw = ImageDraw.Draw(img); font = ImageFont.truetype('arial.ttf', size=48); text = 'XL Title'; text_width, text_height = draw.textsize(text, font=font); draw.text(((1280 - text_width) / 2, (720 - text_height) / 2), text, font=font, fill=(0, 0, 0)); img.save('image.jpg', 'JPEG')"
