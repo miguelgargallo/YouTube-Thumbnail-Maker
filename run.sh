1df7c730e227034431c58686f666e48e0a5a543e
Miguel Gargallo <mgargallo16@gmail.com>
2022-12-27 17:52:06 +0100

    1.0.7 Code Optimization

--- run.sh
+++ run.sh
@@ -3,12 +3,8 @@
 # Assign permissions to the scripts, check if dependencies are installed and install them if not
 chmod +x dependencies.sh && ./dependencies.sh
 
-# Draw a 1280 x 720 pixels white background, with a 16:9 aspect ratio in JPG
-python -c "from PIL import Image; Image.new('RGB', (1280, 720), (255, 255, 255)).save('image.jpg')"
+# Ask the user for a sentence
+read -p "Enter a sentence: " sentence
 
-# Add a sentence to the image in bold, 36px in the middle of the image (centered) by the user from a prompt
-python -c "from PIL import Image, ImageDraw, ImageFont; image = Image.open('image.jpg'); draw = ImageDraw.Draw(image); font = ImageFont.truetype('arial.ttf', 36); draw.text((640, 360), input('Enter a sentence: '), (0, 0, 0), font=font, anchor='mm'); image.save('image.jpg')"
-
-# Save the image with the same input before in a JPG format 100% quality with a 1280 x 720 pixels resolution with a 16:9 aspect ratio
-python -c "from PIL import Image; image = Image.open('image.jpg'); image.save('image.jpg', quality=100, optimize=True, progressive=True, subsampling='4:4:4', dpi=(1280, 720))"
-
+# Create a 1280 x 720 pixels white background with filename $sentence.jpg in the current directory and the sentence in the middle of the image
+python -c "from PIL import Image, ImageDraw, ImageFont; Image.new('RGB', (1280, 720), (255, 255, 255)).save('$sentence.jpg'); image = Image.open('$sentence.jpg'); draw = ImageDraw.Draw(image); font = ImageFont.truetype('arial.ttf', 36); draw.text((640, 360), '$sentence', (0, 0, 0), font=font, anchor='mm'); image.save('$sentence.jpg'); image.save('$sentence.jpg', quality=100, optimize=True, progressive=True, subsampling='4:4:4', dpi=(1280, 720))"
