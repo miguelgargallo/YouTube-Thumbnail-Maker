7d269035d4172e24714f6120d76aaeaca8e343c7
Miguel Gargallo <mgargallo16@gmail.com>
2022-12-28 02:10:19 +0100

    1.0.20 Adding two colors

---
+++ play.sh
@@ -0,0 +1,15 @@
+# Make a bash script to choose between run.sh a random color YouTube Thumbnail Maker, or run christmas red black sh, a Christmas themed YouTube Thumbnail Maker with red and black colors or a third option, run.sh a random color YouTube Thumbnail Maker with a white background and red text.
+
+#!/bin/bash
+echo "Choose between run.sh a random color YouTube Thumbnail Maker, or run christmas red black sh, a Christmas themed YouTube Thumbnail Maker with red and black colors or a third option, run.sh a random color YouTube Thumbnail Maker with a white background and red text."
+echo "Type 1 for run.sh, a random color YouTube Thumbnail Maker"
+echo "Type 2 for run christmas red black themed"
+echo "Type 3 for run christmas red white themed"
+
+read -p "Choose 1, 2, or 3: " choice
+case "$choice" in
+  1) ./run.sh ;;
+  2) ./run_christmas_red_black.sh ;;
+  3) ./run_christmas_red_white.sh ;;
+  *) echo "Invalid option $REPLY" ;;
+esac
